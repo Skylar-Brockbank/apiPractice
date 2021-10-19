be4f984fda24e27a2c5288a6e6aa452ce0ac7857
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-10-19 15:54:34 -0700

    now all cards have javascript to make descriptions appear
    Co-authored-by: Liam Eller <liamthelastson@gmail.com>

--- flashyCards/js/scripts.js
+++ flashyCards/js/scripts.js
@@ -8,6 +8,54 @@ $(document).ready(function() {
   $("#3").click(function() {
     reveal("#3p");
   });
+  $("#4").click(function() {
+    reveal("#4p");
+  });
+  $("#5").click(function() {
+    reveal("#5p");
+  });
+  $("#6").click(function() {
+    reveal("#6p");
+  });
+  $("#7").click(function() {
+    reveal("#7p");
+  });
+  $("#8").click(function() {
+    reveal("#8p");
+  });
+  $("#9").click(function() {
+    reveal("#9p");
+  });
+  $("#10").click(function() {
+    reveal("#10p");
+  });
+  $("#11").click(function() {
+    reveal("#11p");
+  });
+  $("#12").click(function() {
+    reveal("#12p");
+  });
+  $("#13").click(function() {
+    reveal("#13p");
+  });
+  $("#14").click(function() {
+    reveal("#14p");
+  });
+  $("#15").click(function() {
+    reveal("#15p");
+  });
+  $("#16").click(function() {
+    reveal("#16p");
+  });
+  $("#17").click(function() {
+    reveal("#17p");
+  });
+  $("#18").click(function() {
+    reveal("#18p");
+  });
+  $("#19").click(function() {
+    reveal("#19p");
+  });
   function reveal(identifier) {
     $(identifier).toggle();
   }
