3df9ede31972f29890ac538e231e39196e31a6d3
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-10-28 11:53:08 -0700

    break the code, but start the refactoring process
    Co-Authored-by: Mike Watkins <michaelhugheswatkins@gmail.com>
    Co-Authored-by: Albert Lee <leesga8@gmail.com>

--- romanNumerals/js/scripts.js
+++ romanNumerals/js/scripts.js
@@ -4,72 +4,87 @@ function romanNum(number) {
 let runningNumber = number;
 let output = "";
 
-  // if (number===1) {
-  //   return "I"
-  // } else if(number===5) {
-  //   return "V"
-  // } else if(number===10) {
-  //   return "X"
-  // } else if(number===50) {
-  //   return "L"
-  // } else if(number===100) {
-  //   return "C"
-  // } else if(number===500) {
-  //   return "D"
-  // } else if(number===1000) {
-  //   return "M"
-  // } else {
-  //   return "Try typing 1 instead"
-  // }
-  console.log(runningNumber);
-  if (parseInt(runningNumber/1000)>0) {
-    for(let i = parseInt(runningNumber/1000); i > 0; i--) {
-   
-      output = output + "M";
-    }
-    runningNumber = runningNumber - (parseInt(runningNumber/1000) * 1000)
+  let martin = {
+    1000: "M",
+    500: "D",
+    100: "C",
+    50: "L",
+    10: "X",
+    5: "V",
+    1: "I",
+  };
+
+console.log(martin)
+console.log(martin[1])
+console.log(martin[5])
+
+  for(let k in martin){
+    console.log(martin[k]);
+    console.log(k);
   }
 
-  if (parseInt(runningNumber/500)>0) {
-    for(let i = parseInt(runningNumber/500); i > 0; i--) {
-      output = output + "D";
-    }
-    runningNumber = runningNumber - (parseInt(runningNumber/500) * 500)
-  }
-  if (parseInt(runningNumber/100)>0) {
-    for(let i = parseInt(runningNumber/100); i > 0; i--) {
+  for (let k in martin){
+    console.log(martin);
+    if (parseInt(runningNumber/k)>0) {
+      for(let i = parseInt(runningNumber/k); i > 0; i--) {
     
-      output = output + "C";
+        output = output + martin[k];
+      }
+      runningNumber = runningNumber - (parseInt(runningNumber/k) * k)
     }
-    runningNumber = runningNumber - (parseInt(runningNumber/100) * 100)
-  }
-  if (parseInt(runningNumber/50)>0) {
-    for(let i = parseInt(runningNumber/50); i > 0; i--) {
-      output = output + "L";
-    }
-    runningNumber = runningNumber - (parseInt(runningNumber/50) * 50)
-  }
-  if (parseInt(runningNumber/10)>0) {
-    for(let i = parseInt(runningNumber/10); i > 0; i--) {
-      
-      output = output + "X";
-    }
-    runningNumber = runningNumber - (parseInt(runningNumber/10) * 10)
-  }
-  if (parseInt(runningNumber/5)>0) {
-    for(let i = parseInt(runningNumber/5); i > 0; i--) {
-      output = output + "V";
-    }
-    runningNumber = runningNumber - (parseInt(runningNumber/5) * 5)
-  }
-  if (parseInt(runningNumber/1)>0) {
-    for(let i = parseInt(runningNumber/1); i > 0; i--) {
     
-      output = output + "I";
-    }
-    runningNumber = runningNumber - (parseInt(runningNumber/1) * 1)
+
   }
   return output;
 }
+//   if (parseInt(runningNumber/1000)>0) {
+//     for(let i = parseInt(runningNumber/1000); i > 0; i--) {
+   
+//       output = output + "M";
+//     }
+//     runningNumber = runningNumber - (parseInt(runningNumber/1000) * 1000)
+//   }
+
+//   if (parseInt(runningNumber/500)>0) {
+//     for(let i = parseInt(runningNumber/500); i > 0; i--) {
+//       output = output + "D";
+//     }
+//     runningNumber = runningNumber - (parseInt(runningNumber/500) * 500)
+//   }
+//   if (parseInt(runningNumber/100)>0) {
+//     for(let i = parseInt(runningNumber/100); i > 0; i--) {
+    
+//       output = output + "C";
+//     }
+//     runningNumber = runningNumber - (parseInt(runningNumber/100) * 100)
+//   }
+//   if (parseInt(runningNumber/50)>0) {
+//     for(let i = parseInt(runningNumber/50); i > 0; i--) {
+//       output = output + "L";
+//     }
+//     runningNumber = runningNumber - (parseInt(runningNumber/50) * 50)
+//   }
+//   if (parseInt(runningNumber/10)>0) {
+//     for(let i = parseInt(runningNumber/10); i > 0; i--) {
+      
+//       output = output + "X";
+//     }
+//     runningNumber = runningNumber - (parseInt(runningNumber/10) * 10)
+//   }
+//   if (parseInt(runningNumber/5)>0) {
+//     for(let i = parseInt(runningNumber/5); i > 0; i--) {
+//       output = output + "V";
+//     }
+//     runningNumber = runningNumber - (parseInt(runningNumber/5) * 5)
+//   }
+//   if (parseInt(runningNumber/1)>0) {
+//     for(let i = parseInt(runningNumber/1); i > 0; i--) {
+    
+//       output = output + "I";
+//     }
+//     runningNumber = runningNumber - (parseInt(runningNumber/1) * 1)
+//   }
+//   return output;
+// }
 
 //UI Logic
