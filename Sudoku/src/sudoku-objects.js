b92fc3eeb040459af3bbf4724e1323cf7ce492f9
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-11-09 10:28:37 -0800

    test now fails (fixed off by 1 error)
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>

--- Sudoku/src/sudoku-objects.js
+++ Sudoku/src/sudoku-objects.js
@@ -3,7 +3,7 @@ export default function Board() {
   for (let i = 0; i < 9; i++) {
     let row = [];
     for (let j = 0; j < 9; j++) {
-      row.push(j);
+      row.push(j + 1);
     }
     column.push(row);
   }
