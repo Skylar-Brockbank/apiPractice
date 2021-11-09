f88853e77d77f9197733cc55521e573ffcbda0f0
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-11-09 10:29:33 -0800

    changed test to expect values not off by 1
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>

--- Sudoku/__tests__/sudoku-objects.test.js
+++ Sudoku/__tests__/sudoku-objects.test.js
@@ -10,9 +10,9 @@ import Board from '../src/sudoku-objects.js';
 describe('Board',() => {
   test('it should make an object with a 2d array, consisting of 9 9 index long 1d arrays', () => {
     const board1 = new Board();
-    expect(board1.spaces[0][0]).toEqual(0);
-    expect(board1.spaces[0][1]).toEqual(1);
-    expect(board1.spaces[0][2]).toEqual(2);
-    expect(board1.spaces[0][3]).toEqual(3);
+    expect(board1.spaces[0][0]).toEqual(1);
+    expect(board1.spaces[0][1]).toEqual(2);
+    expect(board1.spaces[0][2]).toEqual(3);
+    expect(board1.spaces[0][3]).toEqual(4);
   });
 });
